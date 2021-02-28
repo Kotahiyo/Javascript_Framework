0c40fdfe9e0be38054db04274b98b04e687949ed
Kouhei <hiyotihakou@gmail.com>
2021-03-01 05:30:45 +0900

    link の付け方について

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>記事詳細ページ</div>
+  <div>
+    <div>記事詳細ページ</div>
+    <div>
+      <!-- nuxt-link は必要箇所のみ読み込み = 早い -->
+      <nuxt-link to="/articles/10/edit">Nuxt Link</nuxt-link>
+    </div>
+    <div>
+      <!-- a タグはページ全体を読み込む = nuxt-link に比べ遅い -->
+      <a href="/articles/10/edit">a タグ</a>
+    </div>
+    <div>
+      <nuxt-link to="/articles/10/update"> 更新ページ </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ pages/articles/_id/update.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>更新ページ</div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const foo = 'foo'
+    console.log(foo)
+  },
+}
+</script>
