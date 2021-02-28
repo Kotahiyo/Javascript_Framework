8d28ee2b5a1c01afd58a8c8f086b8c90fdd5ea09
Kouhei <hiyotihakou@gmail.com>
2021-03-01 06:18:20 +0900

    v-記法について

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
   <div>
     <!-- {{}} で囲むと javascript が書ける（マスタッシュ記法） -->
     <div>{{ count }}</div>
+    <!-- v-記法をディレクティブと呼ぶ
+    @ で省略できる
+    @click = v-on:click  -->
+    <button @click="plus">+</button>
+    <button @click="minus">-</button>
   </div>
 </template>
 
@@ -12,8 +17,18 @@ export default {
     // return でオブジェクトのプロパティが count: 0 のものを返している
     // {} で囲んだものはオブジェクトになる（復習）
     return {
-      count: 0,
+      count: 100,
     }
+  },
+  // () の中に : として定義するから
+  // methods は export default のプロパティ
+  methods: {
+    plus() {
+      this.count += 1
+    },
+    minus() {
+      this.count -= 1
+    },
   },
 }
 </script>
