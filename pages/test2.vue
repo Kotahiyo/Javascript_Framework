d2608b11199120ca064ed139eff6578c1dc02bd2
Kouhei <hiyotihakou@gmail.com>
2021-03-02 16:41:51 +0900

     created の値の取り扱いについて

--- pages/test2.vue
+++ pages/test2.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <div>ライフサイクルテスト</div>
-    <v-text-field v-model="number" />
+    <v-text-field ref="number" v-model="number" />
     <div>数値: {{ number }}</div>
+
+    <div :class="$style.container" v-for="user in users" :key="user.id">
+      <div>名前: {{ user.name }}</div>
+      <div>電話番号: {{ user.phone }}</div>
+      <div>メールアドレス: {{ user.email }}</div>
+    </div>
   </div>
 </template>
 
@@ -11,18 +17,33 @@ export default {
   data() {
     return {
       number: 0,
+      users: [],
     }
   },
   created() {
     console.log('created')
+    this.$axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        this.users = response.data
+      })
   },
   mounted() {
     console.log('mounted')
+    this.$refs.number.focus()
   },
   updated() {
     console.log('updated')
+    console.log(this.number)
   },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" module>
+.container {
+  margin: 10px 0;
+  padding: 8px;
+  border: 2px solid #ccc;
+  border-radius: 2px;
+}
+</style>
