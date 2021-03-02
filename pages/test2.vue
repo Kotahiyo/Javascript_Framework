f6e852fec04eb69e36a83cfbe0122ffffef4840d
Kouhei <hiyotihakou@gmail.com>
2021-03-02 15:56:12 +0900

    ライフサイクルフックの基本について学習

---
+++ pages/test2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>ライフサイクルテスト</div>
+    <v-text-field v-model="number" />
+    <div>数値: {{ number }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    }
+  },
+  created() {
+    console.log('created')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  updated() {
+    console.log('updated')
+  },
+}
+</script>
+
+<style lang="scss"></style>
