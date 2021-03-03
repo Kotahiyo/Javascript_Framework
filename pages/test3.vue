2883d57aa10547659738f088ffaaeb2833e89b8c
Kouhei <hiyotihakou@gmail.com>
2021-03-03 09:49:49 +0900

    async, await について学習

---
+++ pages/test3.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>ライフサイクルテスト</div>
+    <v-text-field ref="number" v-model="number" />
+    <div>数値: {{ number }}</div>
+
+    <div :class="$style.container" v-for="user in users" :key="user.id">
+      <div>名前: {{ user.name }}</div>
+      <div>電話番号: {{ user.phone }}</div>
+      <div>メールアドレス: {{ user.email }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+      users: [],
+    }
+  },
+  async created() {
+    const response = await this.$axios.get(
+      'http://jsonplaceholder.typicode.com/users'
+    )
+    console.log(response)
+  },
+  mounted() {
+    console.log('mounted')
+    this.$refs.number.focus()
+  },
+  updated() {
+    console.log('updated')
+    console.log(this.number)
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  margin: 10px 0;
+  padding: 8px;
+  border: 2px solid #ccc;
+  border-radius: 2px;
+}
+</style>
