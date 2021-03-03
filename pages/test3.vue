316e6086ce1d6ed5f14a9551940bead52d071a06
Kouhei <hiyotihakou@gmail.com>
2021-03-03 15:55:24 +0900

    Promise について

--- pages/test3.vue
+++ pages/test3.vue
@@ -20,18 +20,35 @@ export default {
       users: [],
     }
   },
-  async created() {
-    try {
-      const response = await this.$axios.get(
-        'http://jsonplaceholder.typicode.com/userssss'
-      )
-      console.log(response)
-    } catch (err) {
-      const res = err.response
-      console.log(res)
-    }
-
-    // console.log(response)
+  // async created() {
+  //   try {
+  //     const response = await this.$axios.get(
+  //       'http://jsonplaceholder.typicode.com/userssss'
+  //     )
+  //     console.log(response)
+  //   } catch (err) {
+  //     const res = err.response
+  //     console.log(res)
+  //   }
+  // },
+  created() {
+    new Promise((resolve, reject) => {
+      this.$axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((err) => {
+          reject(err.response)
+        })
+    })
+      .then((data) => {
+        console.log(data)
+        this.user = data
+      })
+      .catch((response) => {
+        console.log(response)
+      })
   },
   mounted() {
     console.log('mounted')
